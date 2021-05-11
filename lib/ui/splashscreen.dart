8d3e418417a72fc47e202c08f68bccc8478dfdca
risaaugusta <risa_augusta_26rpl@student.smktelkom-mlg.sch.id>
2021-05-11 17:43:17 +0700

    adding img

---
+++ lib/ui/splashscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  Splashscreen({Key key}) : super(key: key);
+
+  @override
+  _SplashscreenState createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
