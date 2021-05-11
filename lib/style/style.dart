30fcaf177e3e2f845bce59032b1f4ee77fd9132a
risaaugusta <risa_augusta_26rpl@student.smktelkom-mlg.sch.id>
2021-05-11 18:34:19 +0700

    adding splashscreen

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 class Colors{
   const Colors();
 
-  static const Color mainColor = const Color(0xff4182bf);
-  static const Color secondColor = const Color(0xff8ebbe5);
-  static const Color titleColor = const Color(0xff5a606b);
-  static const Color subColor = const Color(0xffcad0db);
+  static const  mainColor = const Color(0xff1D407B);
+  static const  secondColor = const Color(0xff8ebbe5);
+  static const  titleColor = const Color(0xff5a606b);
+  static const  subColor = const Color(0xffcad0db);
+}
+
+class Fonts{
+  static const BOLD= 'PoppinsBold';
+  static const LIGHT = 'PoppinsLight';
+  static const MEDIUM= 'PoppinsMedium';
+  static const REGULAR = 'PoppinsRegular';
 }
--- lib/ui/splashscreen.dart
+++ lib/ui/splashscreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:cakdam/ui/home.dart';
+import 'package:cakdam/style/style.dart';
 
 class Splashscreen extends StatefulWidget {
   Splashscreen({Key key}) : super(key: key);
@@ -8,8 +11,54 @@ class Splashscreen extends StatefulWidget {
 }
 
 class _SplashscreenState extends State<Splashscreen> {
+
+  void initState(){
+    super.initState();
+    splashScreenStart();
+  }
+
+  splashScreenStart() async{
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, (){
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=>Home()),
+      );
+    });
+    }
+
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    throw UnimplementedError();
+//  }
+//  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                height: 350,
+                width: 350,
+                child: Image(image: AssetImage('assets/img/logo.png'))
+            ),
+            Text("Cak Dam",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color(0xff1D407B),
+                    fontSize: 25,
+                    fontFamily: Fonts.BOLD,
+                    fontWeight: FontWeight.bold,
+                )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
