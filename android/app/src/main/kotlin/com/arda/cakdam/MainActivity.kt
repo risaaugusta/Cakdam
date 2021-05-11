5e21ed0f6bfb0911f2bee45368253307c078c5b4
risaaugusta <risa_augusta_26rpl@student.smktelkom-mlg.sch.id>
2021-05-11 16:31:33 +0700

    init project

---
+++ android/app/src/main/kotlin/com/arda/cakdam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arda.cakdam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Colors{
+  const Colors();
+
+  static const Color mainColor = const Color(0xff4182bf);
+  static const Color secondColor = const Color(0xff8ebbe5);
+  static const Color titleColor = const Color(0xff5a606b);
+  static const Color subColor = const Color(0xffcad0db);
+}
